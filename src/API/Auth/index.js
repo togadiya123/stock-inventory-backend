8803bf759849178e4dc9cb72bc9a28fb68f4f31d
Hardik Togadiya <hardiktogadiya123@gmail.com>
2023-10-12 16:58:40 +0530

    Add token validation and update gitignore file
    Added a 'validateToken' function to the authentication controller to verify the validity of a user's token. Also, updated the '.gitignore' file to include the '.env' file and indicate yarn.lock for changes. Now, the newly added 'validateToken' route can be used to confirm a user's authentication status and sensitive information in the '.env' file and the frequently changing 'yarn.lock' file are kept out of version control for security and consistency.

--- src/API/Auth/index.js
+++ src/API/Auth/index.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 
-import {login, signup, logout} from './controller.js';
+import {login, signup, logout, validateToken} from './controller.js';
 import {tokenVerify} from "../../Middleware/index.js";
 
 export const authRouter = Router();
 
 authRouter.route('/login').post(login);
 authRouter.route('/signup').post(signup);
+authRouter.use(tokenVerify).route('/').get(validateToken);
 authRouter.use(tokenVerify).route('/logout').get(logout);
