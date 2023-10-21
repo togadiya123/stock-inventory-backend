2ab802144519fdd81d5700c9a4cdf54173ade1c2
Hardik Togadiya <hardiktogadiya123@gmail.com>
2023-10-21 14:57:31 +0530

    Refactor main.js for better code readability
    Wrapped initial function calls and error handling in a main function, improving overall readability. This refactoring makes the server initialization and database connection more explicit, and it helps in standardizing the application's code structure to follow ECMAScript conventions for better maintainability.

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import { server } from "./Server/index.js";
 import { config } from "./Config/index.js";
 import { connectDatabase } from "./MongoDB/index.js";
 
-try {
-    config();
+const main = async () => {
+    try {
+        config();
 
-    const app = await server();
-    await connectDatabase();
-    app.listena(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    });
-} catch (error) {
-    console.log(error);
-}
+        const app = await server();
+        await connectDatabase();
+        app.listena(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+await main();
